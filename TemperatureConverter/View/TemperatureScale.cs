c2e647c45232d173ff48f6cb5c854254034d5240
pieterjan-dubois <pieterjan.dubois@student.ucll.be>
2021-04-12 17:41:37 +0200

    temperatureconvertor 4th check

--- TemperatureConverter/View/TemperatureScale.cs
+++ TemperatureConverter/View/TemperatureScale.cs
@@ -1,66 +1,57 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace View
+﻿namespace View
 {
-    class TemperatureScale
+    public interface ITemperatureScale
     {
-        public interface ITemperatureScale
-        {
-            string Name { get; }
+        string Name { get; }
 
-       double ConvertToKelvin(double temperature);
-       double ConvertFromKelvin(double temperature);
+        double ConvertToKelvin(double temperature);
+        double ConvertFromKelvin(double temperature);
+    }
+
+    public class KelvinTemperatureScale : ITemperatureScale
+    {
+        public string Name => "Kelvin";
+
+        public double ConvertFromKelvin(double temperature)
+        {
+            return temperature;
         }
 
-        public class KelvinTemperatureScale : ITemperatureScale
+        public double ConvertToKelvin(double temperature)
         {
-            public string Name => "Kelvin";
+            return temperature;
+        }
+    }
 
-            public double ConvertFromKelvin(double temperature)
-            {
-                return temperature;
-            }
 
-            public double ConvertToKelvin(double temperature)
-            {
-                return temperature;
-            }
+
+    public class CelsiusTemperatureScale : ITemperatureScale
+    {
+        public string Name => "Celsius";
+
+        public double ConvertFromKelvin(double temperature)
+        {
+            return temperature - 273.15;
         }
 
-      
-
-        public class CelsiusTemperatureScale : ITemperatureScale
+        public double ConvertToKelvin(double temperature)
         {
-            public string Name => "Celsius";
+            return temperature + 273.15;
+        }
+    }
 
-            public double ConvertFromKelvin(double temperature)
-            {
-                return temperature - 273.15;
-            }
+    public class FahrenheitTemperatureScale : ITemperatureScale
+    {
+        public string Name => "Fahrenheit";
 
-            public double ConvertToKelvin(double temperature)
-            {
-                return temperature + 273.15;
-            }
+        public double ConvertFromKelvin(double temperature)
+        {
+            return (temperature * 1.8) - 459.67;
         }
 
-        public class FahrenheitTemperatureScale : ITemperatureScale
+        public double ConvertToKelvin(double temperature)
         {
-            public string Name => "Fahrenheit";
-
-            public double ConvertFromKelvin(double temperature)
-            {
-               return (temperature * 1.8) - 459.67;
-            }
-
-            public double ConvertToKelvin(double temperature)
-            {
-                return (temperature + 459.67) / 1.8;
-            }
+            return (temperature + 459.67) / 1.8;
         }
     }
 }
