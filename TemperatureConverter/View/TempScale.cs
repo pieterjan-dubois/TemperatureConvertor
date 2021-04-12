d92b34ca59d7c9453a17b3cb9ed6dbea3f3a62ba
pieterjan-dubois <pieterjan.dubois@student.ucll.be>
2021-04-12 18:16:23 +0200

    me stuck

--- TemperatureConverter/View/TempScale.cs
+++ TemperatureConverter/View/TempScale.cs
@@ -1,4 +1,10 @@
-﻿namespace View
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace View
 {
     public interface ITemperatureScale
     {
