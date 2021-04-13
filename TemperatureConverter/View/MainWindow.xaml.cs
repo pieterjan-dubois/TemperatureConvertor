011b70d9669e77f987b8bcad0f9ded18e533b80d
pieterjan-dubois <pieterjan.dubois@student.ucll.be>
2021-04-13 20:36:35 +0200

    temp-scale-vm 1st check

--- TemperatureConverter/View/MainWindow.xaml.cs
+++ TemperatureConverter/View/MainWindow.xaml.cs
@@ -21,27 +21,12 @@ namespace View
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window, INotifyPropertyChanged
+    public partial class MainWindow : Window
     {
-        private double temperatureInKelvin;
-        public event PropertyChangedEventHandler PropertyChanged;
-        public double TemperatureInKelvin
-  {
-       get
-       {
-           return temperatureInKelvin;
-     }
-      set
-       {             
-                temperatureInKelvin = value;
-                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(TemperatureInKelvin)));
-            }
-   }
-
         public MainWindow()
         {
             InitializeComponent();
-            this.DataContext = this;
+            this.DataContext = new ConverterViewModel();
         }
     }
 
--- TemperatureConverter/View/viewModel.cs
+++ TemperatureConverter/View/viewModel.cs
@@ -3,10 +3,74 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.ComponentModel;
 
 namespace View
 {
-    class Class1
+    public class ConverterViewModel : INotifyPropertyChanged
     {
+        private double temperatureInKelvin;
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public ConverterViewModel()
+        {
+            this.Kelvin = new TemperatureScaleViewModel(new KelvinTemperatureScale());
+            this.Celsius = new TemperatureScaleViewModel(new CelsiusTemperatureScale());
+            this.Fahrenheit = new TemperatureScaleViewModel(new FahrenheitTemperatureScale());
+        }
+        public double TemperatureInKelvin
+        {
+            get
+            {
+                return temperatureInKelvin;
+            }
+            set
+            {
+                temperatureInKelvin = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(TemperatureInKelvin)));
+            }
+        }
+        public TemperatureScaleViewModel Kelvin { get; }
+
+        public TemperatureScaleViewModel Celsius { get; }
+
+        public TemperatureScaleViewModel Fahrenheit { get; }
     }
+
+    public class TemperatureScaleViewModel : INotifyPropertyChanged
+    {
+        private readonly ITemperatureScale temperatureScale;
+
+        private readonly ConverterViewModel parent;
+
+
+        public TemperatureScaleViewModel(ConverterViewModel parent, ITemperatureScale temperatureScale)
+        {
+            this.parent = parent;
+            this.temperatureScale = temperatureScale;
+
+            this.parent.PropertyChanged += TemperatureUpdated;
+        }
+
+        public string Name => temperatureScale.Name;
+
+        public double Temperature
+        {
+            get
+            {
+                return temperatureScale.ConvertFromKelvin(parent.TemperatureInKelvin);
+            }
+            set
+            {
+                parent.TemperatureInKelvin = temperatureScale.ConvertFromKelvin(value);
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void TemperatureUpdated(object sender, PropertyChangedEventArgs args)
+       {
+           PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Temperature)));
+      }
+}
 }
