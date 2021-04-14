b9c8bc4eeddacc2cd1f783aacf57f39243652d6b
pieterjan-dubois <pieterjan.dubois@student.ucll.be>
2021-04-14 17:34:45 +0200

    temp-conv-rip done

--- TemperatureConverter/View/MainWindow.xaml.cs
+++ TemperatureConverter/View/MainWindow.xaml.cs
@@ -32,25 +32,6 @@ namespace View
 
 
 
-    public class TemperatureConverter : IValueConverter
-    {
-        public ITemperatureScale TemperatureScale { get; set; }
-     
-
-
-        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
-        {
-            var kelvin = (double)value;
-            return this.TemperatureScale.ConvertFromKelvin(kelvin).ToString();
-
-        }
-
-        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
-        {
-            var temperature = Double.Parse((string)value);
-            return this.TemperatureScale.ConvertToKelvin(temperature);
-
-        }
-    }
+    
 
 }
